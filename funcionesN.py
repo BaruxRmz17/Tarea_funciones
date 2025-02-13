a42b3a3db94e1dfc5f2f1c379a9540ea8c7e8297
Baruc Ramirez <barux111706@gmail.com>
2025-02-13 12:29:37 -0600

    "Funcion final"

--- funcionesN.py
+++ funcionesN.py
@@ -1,6 +1,6 @@
 import sympy as sp
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 #Definimos las variables simbolicas 
 x,y = sp.symbols('x,y')
@@ -23,3 +23,26 @@ f_inv_lambdified = sp.lambdify(y, f_inv[0], 'numpy')
 x_vals = np.linspace(-10,10,20)
 y_vals = f_lambdified(x_vals)
 y_inv_vals = f_inv_lambdified(x_vals)
+
+#Imprimir los valores de la funcion original y su inversa para 20 puntos
+print("\nValores de f(x) para 20 puntos: ")
+for i in range(20):
+    print(f"x = {x_vals[i]:.2f} , f(x) = {y_vals[i]:.2f}, f_inv(y) = {y_inv_vals[i]:.2f}")
+
+#Creamos las graficas
+plt.figure(figsize=(8,6))
+plt.plot(x_vals, y_vals, label="f(x) = x**2 + 2x + 1", color = 'blue')
+plt.plot(y_vals, x_vals, label="f_inv(y) ", color = 'red', linestyle = 'dashed')
+
+#configuracion de la grafica
+plt.title("Funciones y su inversa")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.axhline(0, color='black', linewidth = 0.8)
+plt.axvline(0, color='black', linewidth = 0.8)
+plt.grid(True)
+plt.legend()
+
+#Se muestre la grafica
+plt.show()
+
